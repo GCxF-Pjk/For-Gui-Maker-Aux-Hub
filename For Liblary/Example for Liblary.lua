cf62df8b261a566d85842e1b4590517ce4a43a84
GCxF-Pjk <hvhhhbbbhjj@gmail.com>
2025-09-03 04:11:33 +0800

    Create Example for Liblary.lua

---
+++ For Liblary/Example for Liblary.lua
@@ -0,0 +1,19 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GCxF-Pjk/For-Gui-Maker-Aux-Hub/refs/heads/main/For%20Liblary/Source.lua"))()
+
+-- Create a main window 
+local Main = Library:CreateWindow("Demo GUI")
+
+-- Add a toggle 
+Main:Toggle("Auto Kill", function(state)
+    print("Auto kill state", state) -- your logic
+end)
+
+-- Add a button 
+Main:Button("Click me!", function()
+    print("Clicked!") -- your logic
+end)
+
+-- Add a textbox
+Main:TextBox("Enter player name...", function(text)
+    print("Player name:", text) -- your logic
+end)
